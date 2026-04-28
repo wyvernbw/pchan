e5c1e3f8c67f4c34c9ef32bffddad073e37d22bb
wyvernbw <iamwyvern511@gmail.com>
2026-04-28 22:39:48 +0300

    feat(pchan-dbg-v2): add hexdump panel

--- pchan-dbg-v2/src/main.rs
+++ pchan-dbg-v2/src/main.rs
@@ -1,3 +1,4 @@
+#![feature(iter_array_chunks)]
 #![feature(duration_millis_float)]
 
 use std::{
@@ -20,19 +21,20 @@ use pchan_emu::{
     io::{IO, vblank::VBlank},
 };
 use pchan_gpu::Renderer;
-use pchan_utils::hex;
+use pchan_utils::{Hex, hex, hex_pref};
 use rat_imaginary::{ImageState, ImageWidget};
 use rat_widget::{
     button::{Button, ButtonState},
     event::{HandleEvent, MouseOnly},
     list::{List, ListState, selection::RowSelection},
+    menu::Separator,
     scrolled::Scroll,
 };
 use ratatui::{
     DefaultTerminal, Frame, Terminal, crossterm,
     layout::{Constraint, Layout, Rect},
     style::{Color, Style, Styled},
-    widgets::{Block, BorderType, Row, Table, TableState, Widget},
+    widgets::{Block, BorderType, Borders, ListItem, Row, Table, TableState, Widget},
 };
 use wgpu::Extent3d;
 
@@ -71,6 +73,8 @@ struct TuiState {
     focused:       Focused,
     mips_cursor:   u32,
     mips_range:    RangeInclusive<u32>,
+    mem_cursor:    u32,
+    mem_range:     RangeInclusive<u32>,
 
     mips_jump_to_pc_button: ButtonState,
 }
@@ -100,6 +104,7 @@ enum Focused {
     Preview,
     Registers,
     Mips,
+    Mem,
 }
 
 async fn run_app(env: &EnvVars) -> Result<()> {
@@ -140,6 +145,8 @@ async fn run_app(env: &EnvVars) -> Result<()> {
         mips_cursor:   0x0,
         emu_running:   false,
         mips_range:    0x0..=0x0,
+        mem_cursor:    0xbfc0_0000,
+        mem_range:     0xbfc0_0000..=0xbfc0_0000,
 
         mips_jump_to_pc_button: ButtonState::new(),
     };
@@ -239,9 +246,7 @@ async fn run_app(env: &EnvVars) -> Result<()> {
         tui_state.framebuffer.swap_buffers();
         tui_state.framebuffer.clear_backbuffer();
         tui_state.framebuffer.swap_buffers();
-    });
-
-    Ok(())
+    })
 }
 
 fn run(callback: impl FnOnce(&mut DefaultTerminal)) -> Result<()> {
@@ -316,6 +321,16 @@ fn handle_event(state: &AppState, tui_state: &mut TuiState, ev: &event::Event) {
                 (Focused::Mips, "ctrl+k") => {
                     tui_state.mips_cursor = tui_state.mips_cursor.saturating_sub(4);
                 }
+                (Focused::Mips, "l" | "tab") => tui_state.focused = Focused::Mem,
+                (Focused::Mem, "h" | "backtab") => tui_state.focused = Focused::Mips,
+                (Focused::Mem, mem_key) => match mem_key {
+                    "ctrl+h" => tui_state.mem_cursor = tui_state.mem_cursor.saturating_sub(1),
+                    "ctrl+j" => tui_state.mem_cursor = tui_state.mem_cursor.saturating_add(16),
+                    "ctrl+k" => tui_state.mem_cursor = tui_state.mem_cursor.saturating_sub(16),
+                    "ctrl+l" => tui_state.mem_cursor = tui_state.mem_cursor.saturating_add(1),
+                    _ => {}
+                },
+
                 _ => {}
             }
         }
@@ -340,8 +355,12 @@ fn draw_app(frame: &mut Frame, tui_state: &mut TuiState, state: &AppState) {
     let area = main_block.inner(frame.area());
     main_block.render(frame.area(), frame.buffer_mut());
 
-    let [h1, h2] =
-        Layout::horizontal([Constraint::Ratio(1, 3), Constraint::Ratio(1, 3)]).areas(area);
+    let [h1, h2, h3] = Layout::horizontal([
+        Constraint::Ratio(1, 3),
+        Constraint::Ratio(1, 3),
+        Constraint::Ratio(1, 3),
+    ])
+    .areas(area);
 
     let rest = if let Some(img) = &tui_state.current_frame {
         let ar = img.height() * 100 / img.width();
@@ -373,6 +392,11 @@ fn draw_app(frame: &mut Frame, tui_state: &mut TuiState, state: &AppState) {
 
     draw_register_viewer(rest, frame, tui_state, state);
     draw_mips_assembly(h2, frame, tui_state, state);
+
+    {
+        let [mem_area] = Layout::vertical([Constraint::Percentage(50)]).areas(h3);
+        draw_mem(mem_area, frame, tui_state, state);
+    }
 }
 
 fn draw_register_viewer(area: Rect, frame: &mut Frame, tui_state: &mut TuiState, state: &AppState) {
@@ -394,9 +418,39 @@ fn draw_register_viewer(area: Rect, frame: &mut Frame, tui_state: &mut TuiState,
         .into_iter()
         .chain((0..32).map(|r| Row::new([reg_str(r), regs_hex[r as usize].as_str()])));
     let table = Table::new(rows, [Constraint::Length(3), Constraint::Length(10)])
-        .row_highlight_style(Style::new().bg(tui_state.theme.primary).bold())
+        .row_highlight_style(Style::new().bg(tui_state.theme.primary).bold().italic())
         .theme(&tui_state.theme);
     frame.render_stateful_widget(table, area, &mut tui_state.reg_list);
+}
+
+/// returns the offset
+fn compute_infinite_list(
+    cursor: &mut u32,
+    page: &mut RangeInclusive<u32>,
+    to_grab: u32,
+    stride: u32,
+    full_page_movement: bool,
+) -> usize {
+    if !page.contains(cursor) {
+        let start = *page.start();
+        let end = *page.end();
+        if *cursor >= end {
+            let offset = match full_page_movement {
+                false => cursor.saturating_sub(end),
+                true => to_grab,
+            };
+            let end = end.saturating_add(offset);
+            *page = (end.saturating_sub(to_grab * stride))..=end;
+        } else if *cursor <= start {
+            let offset = match full_page_movement {
+                false => start.saturating_sub(*cursor),
+                true => to_grab,
+            };
+            let start = start.saturating_sub(offset);
+            *page = start..=(start.saturating_add(to_grab * stride));
+        }
+    };
+    (*cursor - page.start()) as usize
 }
 
 fn draw_mips_assembly(area: Rect, frame: &mut Frame, tui_state: &mut TuiState, state: &AppState) {
@@ -410,8 +464,11 @@ fn draw_mips_assembly(area: Rect, frame: &mut Frame, tui_state: &mut TuiState, s
                 hex(*tui_state.mips_range.start()),
                 hex(*tui_state.mips_range.end())
             ))
-            .theme(&tui_state.theme)
-            .focus_style(tui_state, Focused::Mips);
+            .border_style(
+                Style::new()
+                    .with_theme(&tui_state.theme)
+                    .with_focus(tui_state, Focused::Mips),
+            );
         let a = block.inner(area);
         frame.render_widget(block, area);
         a
@@ -420,30 +477,22 @@ fn draw_mips_assembly(area: Rect, frame: &mut Frame, tui_state: &mut TuiState, s
         Layout::vertical([Constraint::Length(3), Constraint::Fill(1)]).areas(area);
 
     {
+        let [button_area] =
+            Layout::horizontal([Constraint::Max("jump to pc".len() as u16 + 4)]).areas(button_area);
         let button = Button::new("jump to pc")
             .block(Block::bordered().border_type(BorderType::Rounded))
             .armed_style(tui_state.theme.primary);
         frame.render_stateful_widget(button, button_area, &mut tui_state.mips_jump_to_pc_button);
     }
 
-    let to_grab = list_area.height as u32;
+    let offset = compute_infinite_list(
+        &mut tui_state.mips_cursor,
+        &mut tui_state.mips_range,
+        list_area.height as u32,
+        4,
+        false,
+    );
     let mut table_state = TableState::new();
-    if !tui_state.mips_range.contains(&tui_state.mips_cursor) {
-        let start = *tui_state.mips_range.start();
-        let end = *tui_state.mips_range.end();
-        if tui_state.mips_cursor >= end {
-            let offset = tui_state.mips_cursor.saturating_sub(end);
-            let end = end.saturating_add(offset);
-            tui_state.mips_range = (end.saturating_sub(to_grab * 4))..=end;
-        } else if tui_state.mips_cursor <= start {
-            let offset = start.saturating_sub(tui_state.mips_cursor);
-            let start = start.saturating_sub(offset);
-            tui_state.mips_range = start..=(start.saturating_add(to_grab * 4));
-        }
-    };
-    let offset = tui_state
-        .mips_cursor
-        .saturating_sub(*tui_state.mips_range.start());
     table_state = table_state.with_selected(offset as usize >> 2);
     let items = tui_state
         .mips_range
@@ -458,22 +507,144 @@ fn draw_mips_assembly(area: Rect, frame: &mut Frame, tui_state: &mut TuiState, s
                 value.unwrap_or(DecodedOp::Illegal(pchan_emu::dynarec_v2::emitters::Illegal)),
             )
         })
-        .map(|(addr, op)| Row::new([hex(addr).to_string(), op.to_string()]));
+        .map(|(addr, op)| {
+            let mut decoded_op = op.to_string();
+            if addr == state.emu.cpu.pc {
+                use std::fmt::Write;
+                _ = write!(decoded_op, " <-");
+            };
+            Row::new([hex(addr).to_string(), decoded_op])
+        });
     let list = Table::new(items, [Constraint::Length(10), Constraint::Fill(1)])
         .theme(&tui_state.theme)
-        .row_highlight_style(Style::new().bg(tui_state.theme.primary).bold());
+        .row_highlight_style(Style::new().bg(tui_state.theme.primary).bold().italic());
     frame.render_stateful_widget(list, list_area, &mut table_state);
+}
+
+fn draw_mem(area: Rect, frame: &mut Frame, tui_state: &mut TuiState, state: &AppState) {
+    let area = {
+        let block = Block::bordered()
+            .border_type(BorderType::Rounded)
+            .title("+ mem +")
+            .title_top(format!(
+                "+ {} in {}..{} +",
+                hex(tui_state.mem_cursor),
+                hex(*tui_state.mem_range.start()),
+                hex(*tui_state.mem_range.end())
+            ))
+            .border_style(
+                Style::new()
+                    .with_theme(&tui_state.theme)
+                    .with_focus(tui_state, Focused::Mem),
+            );
+        let a = block.inner(area);
+        frame.render_widget(block, area);
+        a
+    };
+    if tui_state.mem_range.start() == tui_state.mem_range.end() {
+        tui_state.mem_range =
+            *tui_state.mem_range.start()..=(*tui_state.mem_range.start() + area.height as u32 * 16);
+    }
+    let offset = compute_infinite_list(
+        &mut tui_state.mem_cursor,
+        &mut tui_state.mem_range,
+        area.height as u32 * 16,
+        1,
+        true,
+    );
+
+    let mut table_state = TableState::new();
+    let hexdump = tui_state
+        .mem_range
+        .clone()
+        .step_by(0x4)
+        .array_chunks::<4>()
+        .map(|addr| {
+            let values = addr.map(|addr| {
+                let value = IO::try_read_pure::<u32>(&state.emu, addr).unwrap_or(0x0);
+
+                value
+                    .to_le_bytes()
+                    .map(|byte| hex_pref::<_, false>(byte).to_string())
+            });
+            Row::new(values.into_iter().flatten())
+        });
+    table_state = table_state.with_selected_cell((offset >> 4, tui_state.mem_cursor as usize % 16));
+    let hexdump_width = 9 * 4;
+    let hexdump = Table::new(
+        hexdump,
+        [
+            Constraint::Length(2),
+            Constraint::Length(2),
+            Constraint::Length(2),
+            Constraint::Length(3),
+        ]
+        .repeat(4),
+    )
+    .theme(&tui_state.theme)
+    .cell_highlight_style(Style::new().bg(tui_state.theme.primary).bold().italic())
+    .column_spacing(0);
+    let [hexdump_area, sep_area, inspect_area] = Layout::horizontal([
+        Constraint::Length(hexdump_width),
+        Constraint::Max(1),
+        Constraint::Fill(1),
+    ])
+    .areas(area);
+    frame.render_stateful_widget(hexdump, hexdump_area, &mut table_state);
+
+    Block::new()
+        .borders(Borders::LEFT)
+        .border_type(BorderType::LightTripleDashed)
+        .render(sep_area, frame.buffer_mut());
+
+    {
+        let value_u32 = IO::try_read_pure::<u32>(&state.emu, tui_state.mem_cursor).unwrap_or(0x0);
+        let value_u16 = IO::try_read_pure::<u16>(&state.emu, tui_state.mem_cursor).unwrap_or(0x0);
+        let value_u8 = IO::try_read_pure::<u8>(&state.emu, tui_state.mem_cursor).unwrap_or(0x0);
+        let ascii = IO::try_read_pure::<u32>(&state.emu, tui_state.mem_cursor).unwrap_or(0x0);
+        let ascii = ascii
+            .to_le_bytes()
+            .map(|byte| char::from_u32(byte as u32).unwrap_or_default());
+        frame.render_widget(
+            ratatui::widgets::List::new([
+                ratatui::prelude::Line::from_iter(["u32 ", hex(value_u32).as_str()]),
+                ratatui::prelude::Line::from_iter(["u16 ", hex(value_u16).as_str()]),
+                ratatui::prelude::Line::from_iter([" u8 ", hex(value_u8).as_str()]),
+                ratatui::prelude::Line::from_iter([
+                    "ascii ",
+                    &format!("{}{}{}{}", ascii[0], ascii[1], ascii[2], ascii[3]),
+                ]),
+            ]),
+            inspect_area,
+        );
+    }
 }
 
 trait Themed: Styled + Sized {
     fn theme(self, theme: &Theme) -> Self::Item {
-        self.set_style(Style::new().fg(theme.fg))
+        self.set_style(Style::new().with_theme(theme))
     }
     fn focus_style(self, tui_state: &TuiState, value: Focused) -> Self::Item {
+        let style = self.style();
+        self.set_style(style.with_focus(tui_state, value))
+    }
+}
+
+trait StyleExt {
+    fn with_theme(self, theme: &Theme) -> Self;
+    fn with_focus(self, tui_state: &TuiState, value: Focused) -> Self;
+}
+
+impl StyleExt for Style {
+    fn with_theme(self, theme: &Theme) -> Self {
+        self.fg(theme.fg)
+    }
+
+    fn with_focus(self, tui_state: &TuiState, value: Focused) -> Self {
         if tui_state.focused == value {
-            self.set_style(Style::new().fg(tui_state.theme.primary))
+            self.fg(tui_state.theme.primary)
         } else {
-            self.theme(&tui_state.theme)
+            self.with_theme(&tui_state.theme)
         }
     }
 }
